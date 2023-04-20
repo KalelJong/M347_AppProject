6514dff9c9bde7b7ff08a43a9d36825e9ffec42d
Zakria Ahmed Samma <zakria.samma@edu.tbz.ch>
2023-04-20 16:41:52 +0200

    DB implemented

--- server/index.js
+++ server/index.js
@@ -35,6 +35,6 @@ io.on("connection", (socket) => {
     console.log("User Disconnected", socket.id);
   });
 });
-server.listen(3001, () => {
+server.listen(3000, () => {
   console.log("SERVER RUNNING");
 });

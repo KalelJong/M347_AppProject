1616234b699c2f117ecc6b3f07a49376b72db68e
Kalel Poeta <kalel.poeta@orbisag.ch>
2023-05-07 16:24:31 +0200

    Changes to sql script with AI Syntax corrector

--- server/index.js
+++ server/index.js
@@ -1,28 +1,38 @@
-import pool from "./db.js"
+import pool from "./db.js";
+import mariadb from "mariadb";
+
 const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
-app.use(cors());  
+app.use(cors());
+
+const pool = mariadb.createPool({
+  host: "chat-db",
+  user: "root",
+  password: "Password1",
+  database: "ChatDB",
+});
 
 app.get("/chat/:name", async (req, res) => {
   let conn;
-  try
-  {
+  try {
     conn = await pool.getConnection();
-
-    let sql = `SELECT * FROM desserts`;
-    let result = await conn.query(sql);
-    res.send(result);
-  } catch (error) {
-        throw error
-    } finally {
-        if (conn) {
-          conn.release()
-        }
-    }
+    const rows = await conn.query("SELECT 1 as val");
+    console.log(rows); //[ {val: 1}, meta: ... ]
+    const res = await conn.query("INSERT INTO myTable value (?, ?)", [
+      1,
+      "mariadb",
+    ]);
+    console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) return conn.end();
+  }
 });
+
 // const server = http.createServer(app);
 // const io = new Server(server, {
 // });
